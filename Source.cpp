9f708e8d344db9e4b9706ad71f22792cd73f8a28
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2018-02-28 20:06:55 +0100

    All files

---
+++ Source.cpp
@@ -0,0 +1,57 @@
+#include "SDL/include/SDL.h"
+#pragma comment(lib, "SDL/libx86/SDL2.lib")
+#pragma comment(lib, "SDL/libx86/SDL2main.lib")
+
+
+int main(int arfc, char* argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow("Rectangle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		600, 400, SDL_WINDOW_OPENGL);
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+		SDL_RENDERER_PRESENTVSYNC);
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_Rect rect = { 0, 0, 60, 40 };
+
+	SDL_RenderFillRect(renderer, &rect);
+	SDL_RenderPresent(renderer);
+
+	int x = 0;
+	int y = 0;
+
+	while (1)
+	{
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderFillRect(renderer, &rect);
+
+		if (x == 0 && rect.x <= 540) rect.x += 2;
+		else x = 1;
+
+		if (x == 1 && rect.x >= 0) rect.x -= 2;
+		else x = 0;
+
+		if (y == 0 && rect.y <= 360) rect.y += 2;
+		else y = 1;
+
+		if (y == 1 && rect.y >= 0) rect.y -= 2;
+		else y = 0;
+
+		rect = { rect.x, rect.y, 60, 40 };
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
